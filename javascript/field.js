e8968c7f49b1a910803588d778eb292641b00bae
FLinguenheld <f@linguenheld.fr>
2022-03-13 17:10:21 +0100

    Add css

--- javascript/field.js
+++ javascript/field.js
@@ -18,17 +18,32 @@ export class BaseField{
                     })
     }
 
+    /* Saves the new text and shows */
     setText(text){
         this._text = text
-        this.showText()
+        this.showText(false)
     }
 
-    showText(){
+    /* Displays the text value.
+     * Use clicked to directely modify container's class name */
+    showText(clicked=true){
         Dom.clearContainer(this._container)
         Dom.addElemWithText('p', this._text, this._container)
+        this.setClicked(clicked)
+    }
+
+    /* Removes/adds classname's _container
+     * Allows to play with css values */
+    setClicked(yes=true){
+        if(yes){
+            this._container.classList.remove('waiting')
+            this._container.classList.add('clicked')
+        }else{
+            this._container.classList.remove('clicked')
+            this._container.classList.add('waiting')
+        }
     }
 }
-
 
 
 /* Specialized to display clickable fields */
@@ -45,16 +60,16 @@ export class Field extends BaseField{
                     })
     }
 
+    /* Overload to save the next nonetheless with displaying the title instead of text */
     setText(text){
         this._text = text
         this.showTitle()
     }
 
+    /* Displays the title and changes the container's class name*/
     showTitle(){
         Dom.clearContainer(this._container)
         Dom.addElemWithText('p', this.#title, this._container)
+        this.setClicked(false)
     }
-
-
 }
-
--- javascript/main.js
+++ javascript/main.js
@@ -2,37 +2,56 @@ import {Words} from "./words.js"
 import {BaseField, Field} from "./field.js"
 
 
-
 // −− GET WORDS −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 const words = new Words()
 await words.init()
 
-
 // −− FIELDS −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 const word = new BaseField('word')
-const translation = new Field('translation', 'Traduction')
-const synonym = new Field('synonym', 'Synonyme')
-const context = new Field('context', 'Contexte')
-const comment = new Field('comment', 'Commentaire')
-
+const translation = new Field('translation', 'Translation')
+const synonym = new Field('synonym', 'Synonym')
+const context = new Field('context', 'Context')
+const comment = new Field('comment', 'Comment')
 
 // −− SET WORD CLICK EVENT  −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-let f = function showAll(){
-    translation.showText()
-    synonym.showText()
-    context.showText()
-    comment.showText()
+word.setEvent(function showAll(){
+    word.showText(true)
+    translation.showText(true)
+    synonym.showText(true)
+    context.showText(true)
+    comment.showText(true)
+})
+
+// −− BT REVERSE −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+let englishToFrench = true
+
+const btReverse = document.getElementById('reverse')
+btReverse.addEventListener('click', () => {
+                                    englishToFrench = !englishToFrench
+                                          }
+                          )
+
+// −− BT NEXT −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+function next(){
+
+    const newWord = words.random()
+
+    if (englishToFrench){
+        word.setText(newWord[0])
+        translation.setText(newWord[1])
+    }else{
+        word.setText(newWord[1])
+        translation.setText(newWord[0])
+    }
+    synonym.setText(newWord[2])
+    context.setText(newWord[3])
+    comment.setText(newWord[4])
 }
 
-word.setEvent(f)
+const btNext = document.getElementById('next')
+btNext.addEventListener('click', () => {
+                next()
+                })
 
-
-// −− TEST −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-const newWord = words.random()
-console.log(newWord)
-
-word.setText(newWord[0])
-translation.setText(newWord[1])
-synonym.setText(newWord[2])
-context.setText(newWord[3])
-comment.setText(newWord[4])
+// −− FIRST −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+next()
